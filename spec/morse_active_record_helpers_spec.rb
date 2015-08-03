b0e69b5fba316f90ab392de71c7b44018fca721b
Terry S <itsterry@gmail.com>
2015-08-03 13:28:44 +0100

    First implementation

--- spec/morse_active_record_helpers_spec.rb
+++ spec/morse_active_record_helpers_spec.rb
@@ -1,11 +1,41 @@
 require 'spec_helper'
 
 describe MorseActiveRecordHelpers do
-  it 'has a version number' do
-    expect(MorseActiveRecordHelpers::VERSION).not_to be nil
+  describe "version number" do
+    it 'has a version number' do
+      expect(MorseActiveRecordHelpers::VERSION).not_to be nil
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe "associated_object_exists(klass,fn,required=true)" do
+    skip "TODO"
+  end
+
+  describe "blank_to_nil(thing)" do
+    skip "TODO"
+  end
+
+  describe "process_attachment(options={})" do
+    skip "TODO"
+  end
+
+  describe "survive_if(thing)" do
+    skip "TODO"
+  end
+
+  describe "there_can_be_only_one(thing)" do
+    skip "TODO"
+  end
+
+  describe "there_must_be_one(thing)" do
+    skip "TODO"
+  end
+
+  describe "validate_integer_or_default(thing,default)" do
+    skip "TODO"
+  end
+
+  describe "validate_mandatory_boolean(thing,message='must be true')" do
+    skip "TODO"
   end
 end
